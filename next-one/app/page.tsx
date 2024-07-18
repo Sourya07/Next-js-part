b8c144145fa59166dd2df796d6e9b01d7a43f52a
Sourya07 <singhsourya137@gmail.com>
2024-07-18 23:35:05 +0530

    h

--- next-one/app/page.tsx
+++ next-one/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main >
       <div>this my latest  projects</div>
+
+      <div>
+        adding new file after this
+      </div>
     </main>
   );
 }
